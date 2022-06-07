3955e0be22b82e935e518b9e2e1403d81cd79053
MohammadBaniAlmarjeh <moh.almarjh@gmail.com>
2022-06-07 19:28:54 +0300

    add models

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,50 +16,94 @@ st.set_page_config(
 rtl = lambda w: get_display(f"{arabic_reshaper.reshape(w)}")
 
 
-_, col1, _ = st.columns(3)
-
-with col1:
-    st.image("svu.png", width=200)
-    st.title("تلخيص النصوص باللغة العربية")
+st.title("تَلْخِيصُ اَلنُّصُوصِ بِاللُّغَةِ اَلْعَرَبِيَّةِ")
 
 st.markdown(
     """
 <style>
-p, div, input, label {
-  text-align: right;
+@import url(https://fonts.googleapis.com/earlyaccess/scheherazade.css);
+section.main {
+    background-color: beige;
 }
+.stMarkdown h1, .main .element-container.css-o7ulmj.e1tzin5v3 {
+    text-align: right;
+}
+.stMarkdown div.css-nlntq9.e16nr0p33 {
+    font-weight: bold;
+}
+textarea {
+    direction: rtl;
+    height: 140px;
+}
+.stTextArea .css-qrbaxs {
+    float: right;
+    font-size: 23px;
+}
+h1 {
+    font-family: 'Scheherazade', serif;
+}
+
+
+.main div.css-nlntq9.e16nr0p33 > p {
+    direction: rtl;
+}
+.main .stMarkdown div.css-nlntq9 p {
+    font-size: 22px;
+}
+.main .stMarkdown div.css-nlntq9 {
+    direction: rtl;
+}
+.main p, .main div, .main input, .main label {
+  text-align: right;
+  direction: rtl;
+}
+.main  div>h1>div {
+    left: 0;
+}
+.main button {
+    font-size: 22px;
+}
+
 </style>
     """,
     unsafe_allow_html=True,
 )
 
-st.sidebar.header("Info")
-st.sidebar.write("Made by Mohammad Bani Almarjeh")
+#st.sidebar.write(
+#    "Source Code [GitHub](https://github.com/)"
+#)
 st.sidebar.image("svu.png", width=150)
-st.sidebar.write(
-    "Source Code [GitHub](https://github.com/)"
+st.sidebar.write("\n")
+st.sidebar.write("Arabic Text Summarization")
+st.sidebar.write("Made by Mohammad Bani Almarjeh")
+st.sidebar.write("\n")
+
+model_selected = st.sidebar.selectbox(
+     'Select a Model',
+     ('T5','BERT2BERT', 'GPT-2', 'mBERT2mBERT','Transformer'))
+st.sidebar.write("\n")
+num_beams = st.sidebar.slider(
+    "Number of beams", min_value=1, max_value=10, value=3, step=1
 )
 st.sidebar.write("\n")
-option = st.sidebar.selectbox(
-     'Select Model',
-     ('BERT2BERT', 'GPT2', 'Transformer', 'Seq2Seq_LSTM'))
-
-st.sidebar.write('You selected:', option)
-st.sidebar.write("\n")
-n_answers = st.sidebar.slider(
-    "Max. number of answers", min_value=1, max_value=10, value=2, step=1
+length_penalty = st.sidebar.slider(
+    "Length penalty ", min_value=0.1, max_value=3.0, value=1.0, step=0.1
 )
 
-text = st.text_area("", value="محفزات ومزايا وإجراءات كثيرة قدمتها الجهات المعنية لتشجيع الاستثمارات في مجال الطاقات المتجددة كإحداث صندوق تمويل مشاريع الطاقات البديلة وقانون الاستثمار رقم 18 للعام 2021 ما شجع على إقبال العديد من المستثمرين على هذه المشاريع في ظل الحاجة الملحة لمصادر إضافية بديلة متجددة تدعم المنظومة الكهربائية في سورية.")
-model_selected = 2
-run_query = st.button("لخصّ")
-if run_query:
+#st.sidebar.write("\n")
+#st.sidebar.write("Made by Mohammad Bani Almarjeh")
+
+text = st.text_area("أدخل نص ليتم تلخيصه", value="شهدت مدينة طرابلس، مساء أمس الأربعاء، احتجاجات شعبية وأعمال شغب لليوم الثالث على التوالي، وذلك بسبب تردي الوضع المعيشي والاقتصادي. واندلعت مواجهات عنيفة وعمليات كر وفر ما بين الجيش اللبناني والمحتجين استمرت لساعات، إثر محاولة فتح الطرقات المقطوعة، ما أدى إلى إصابة العشرات من الطرفين.")
+
+run_query = st.button("لخّص")
+if run_query: #and model_selected != 'Seq2Seq_LSTM':
     # https://discuss.streamlit.io/t/showing-a-gif-while-st-spinner-runs/5084
     with st.spinner("جاري التلخيص ..."):
-        result = get_results(text, model_selected)
-
+        result = get_results(text, model_selected, num_beams, length_penalty)
     if len(result) > 0:
-        st.write("الملخص:")
+        #st.write("الملخص:")
         st.write(result)
     else:
         st.write("")
+#elif run_query and model_selected == 'Seq2Seq_LSTM':
+#    st.write("عذراً, هذا النموذج غير متاح حالياً")
--- summarize.py
+++ summarize.py
@@ -6,55 +6,117 @@ from urllib.parse import unquote
 
 import streamlit as st
 from codetiming import Timer
-from transformers import AutoTokenizer, pipeline
+from transformers import pipeline
 from preprocess import ArabertPreprocessor
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("malmarjeh/bert2bert")
-
-model = AutoModelForSeq2SeqLM.from_pretrained("malmarjeh/bert2bert")
-
-generation_pipeline = pipeline("text2text-generation",model=model,tokenizer=tokenizer)
-
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
+from transformers import GPT2TokenizerFast, BertTokenizer
 
 logger = logging.getLogger(__name__)
-
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+logger.info("Loading models...")
+reader_time = Timer("loading", text="Time: {:.2f}", logger=logging.info)
+reader_time.start()
+#####
+
+tokenizer = AutoTokenizer.from_pretrained("malmarjeh/bert2bert")
+model = AutoModelForSeq2SeqLM.from_pretrained("malmarjeh/bert2bert")
+generation_pipeline = pipeline("text2text-generation",model=model,tokenizer=tokenizer)
+logger.info("BERT2BERT is loaded")
+
+tokenizer_mbert = BertTokenizer.from_pretrained("malmarjeh/mbert2mbert-arabic-text-summarization")
+model_mbert = AutoModelForSeq2SeqLM.from_pretrained("malmarjeh/mbert2mbert-arabic-text-summarization")
+generation_pipeline_mbert = pipeline("text2text-generation",model=model_mbert,tokenizer=tokenizer_mbert)
+logger.info("mBERT2mBERT is loaded")
+
+tokenizer_t5 = AutoTokenizer.from_pretrained("malmarjeh/t5-arabic-text-summarization")
+model_t5 = AutoModelForSeq2SeqLM.from_pretrained("malmarjeh/t5-arabic-text-summarization")
+generation_pipeline_t5 = pipeline("text2text-generation",model=model_t5,tokenizer=tokenizer_t5)
+logger.info("T5 is loaded")
+
+tokenizer_transformer = AutoTokenizer.from_pretrained("malmarjeh/transformer")
+model_transformer = AutoModelForSeq2SeqLM.from_pretrained("malmarjeh/transformer")
+generation_pipeline_transformer = pipeline("text2text-generation",model=model_transformer,tokenizer=tokenizer_transformer)
+logger.info("Transformer is loaded")
+
+tokenizer_gpt2 = GPT2TokenizerFast.from_pretrained("aubmindlab/aragpt2-base")
+model_gpt2 = AutoModelForCausalLM.from_pretrained("malmarjeh/gpt2")
+logger.info("GPT-2 is loaded")
+
+reader_time.stop()
+
 preprocessor = ArabertPreprocessor(model_name="")
-logger.info("Loading Pipeline...")
 
-logger.info("Finished loading Pipeline...")
+logger.info("Finished loading the models...")
+logger.info(f"Time spent loading: {reader_time.last}")
 
-
-@lru_cache(maxsize=100)
-def get_results(text, model=4):
+@lru_cache(maxsize=200)
+def get_results(text, model_selected, num_beams, length_penalty):
     logger.info("\n=================================================================")
-    logger.info(f"Question: {text}")
-
+    logger.info(f"Text: {text}")
+    logger.info(f"model_selected: {model_selected}")
+    logger.info(f"length_penalty: {length_penalty}")
     reader_time = Timer("summarize", text="Time: {:.2f}", logger=logging.info)
     reader_time.start()
-
-    text = preprocessor.preprocess(text)
-
-    if model == 2:
-        result = generate_transformer(text)
-    elif model == 3:
-        result = generate_gpt2(text)
-    else:
+    logger.info(len(preprocessor.preprocess(text).split()))
+    text = ' '.join(preprocessor.preprocess(text).split()[:150])
+    
+    if model_selected == 'Transformer':
+        result = generation_pipeline_transformer(text,
+            pad_token_id=tokenizer_transformer.eos_token_id,
+            num_beams=num_beams,
+            repetition_penalty=3.0,
+            max_length=200,
+            length_penalty=length_penalty,
+            no_repeat_ngram_size = 3)[0]['generated_text']
+        logger.info('Transformer')
+    #elif model_selected == 'Seq2Seq_LSTM':
+        #result = generate_seq2seq(text, num_beams)
+    #    result = "عذراً، هذا النموذج غير متاح حالياً"
+    #    logger.info('Seq2Seq_LSTM')
+    elif model_selected == 'GPT-2':
+        text_processed = '\n النص: ' + text + ' \n الملخص: \n '
+        tokenizer_gpt2.add_special_tokens({'pad_token': '<pad>'})
+        text_tokens = tokenizer_gpt2.batch_encode_plus([text_processed], return_tensors='pt', padding='max_length', max_length=150)
+        output_ = model_gpt2.generate(input_ids=text_tokens['input_ids'],repetition_penalty=3.0,length_penalty=length_penalty, num_beams=num_beams, max_length=240, pad_token_id=2, eos_token_id=0, bos_token_id=10611)
+        result = tokenizer_gpt2.decode(output_[0][150:], skip_special_tokens=True).strip()
+        logger.info('GPT-2')
+    elif model_selected == 'mBERT2mBERT':
+        result = generation_pipeline_mbert(text,
+            pad_token_id=tokenizer_mbert.eos_token_id,
+            num_beams=num_beams,
+            repetition_penalty=3.0,
+            max_length=200,
+            length_penalty=length_penalty,
+            no_repeat_ngram_size = 3)[0]['generated_text']
+        logger.info('mBERT')
+    elif model_selected == 'T5':
+        result = generation_pipeline_t5(text,
+            pad_token_id=tokenizer_t5.eos_token_id,
+            num_beams=num_beams,
+            repetition_penalty=3.0,
+            max_length=200,
+            length_penalty=length_penalty,
+            no_repeat_ngram_size = 3)[0]['generated_text']
+        logger.info('t5')
+    elif model_selected == 'BERT2BERT':
         result = generation_pipeline(text,
             pad_token_id=tokenizer.eos_token_id,
-            num_beams=3,
+            num_beams=num_beams,
+            repetition_penalty=3.0,
             max_length=200,
-            top_p=0.9,
-            repetition_penalty = 3.0,
+            length_penalty=length_penalty,
             no_repeat_ngram_size = 3)[0]['generated_text']
+        logger.info('bert2bert')
+    else:
+        result = "الرجاء اختيار نموذج"
+    #repetition_penalty = 3.0,
 
     reader_time.stop()
     logger.info(f"Time spent: {reader_time.last}")
+
     return result
 
 
-
 if __name__ == "__main__":
-    results_dict = get_results("ما هو نظام لبنان؟")
+    results_dict = "" #get_results("")
