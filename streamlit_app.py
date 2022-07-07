898d6763160604f8f4f2a3f9b1ad98b8490cf586
MohammadBaniAlmarjeh <moh.almarjh@gmail.com>
2022-07-07 23:52:06 +0300

    update and fix an issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import arabic_reshaper
 import streamlit as st
 from bidi.algorithm import get_display
 
-from summarize import get_results
-
 st.set_page_config(
     page_title="Arabic Text Summarization",
     page_icon="📖",
@@ -13,8 +11,9 @@ st.set_page_config(
     # layout="wide"
 )
 
-rtl = lambda w: get_display(f"{arabic_reshaper.reshape(w)}")
+from summarize import get_results
 
+rtl = lambda w: get_display(f"{arabic_reshaper.reshape(w)}")
 
 st.title("تَلْخِيصُ اَلنُّصُوصِ بِاللُّغَةِ اَلْعَرَبِيَّةِ")
 
@@ -43,7 +42,6 @@ h1 {
     font-family: 'Scheherazade', serif;
 }
 
-
 .main div.css-nlntq9.e16nr0p33 > p {
     direction: rtl;
 }
@@ -69,41 +67,39 @@ h1 {
     unsafe_allow_html=True,
 )
 
-#st.sidebar.write(
-#    "Source Code [GitHub](https://github.com/)"
-#)
 st.sidebar.image("svu.png", width=150)
 st.sidebar.write("\n")
 st.sidebar.write("Arabic Text Summarization")
-st.sidebar.write("Made by Mohammad Bani Almarjeh")
+st.sidebar.write("Made by Mohammad Bani Almarjeh ([Linkedin](https://www.linkedin.com/in/mohammad-bani-almarjeh))")
 st.sidebar.write("\n")
 
 model_selected = st.sidebar.selectbox(
-     'Select a Model',
+     'Select a model',
      ('T5','BERT2BERT', 'GPT-2', 'mBERT2mBERT','Transformer'))
 st.sidebar.write("\n")
 num_beams = st.sidebar.slider(
     "Number of beams", min_value=1, max_value=10, value=3, step=1
 )
+
+length_pe_slider_disabled = False
+if model_selected == "GPT2":
+    length_pe_slider_disabled = True
+
 st.sidebar.write("\n")
 length_penalty = st.sidebar.slider(
-    "Length penalty ", min_value=0.1, max_value=3.0, value=1.0, step=0.1
+    "Length penalty ", min_value=0.1, max_value=3.0, value=1.0, step=0.1, disabled=length_pe_slider_disabled
 )
 
-#st.sidebar.write("\n")
-#st.sidebar.write("Made by Mohammad Bani Almarjeh")
-
-text = st.text_area("أدخل نص ليتم تلخيصه", value="شهدت مدينة طرابلس، مساء أمس الأربعاء، احتجاجات شعبية وأعمال شغب لليوم الثالث على التوالي، وذلك بسبب تردي الوضع المعيشي والاقتصادي. واندلعت مواجهات عنيفة وعمليات كر وفر ما بين الجيش اللبناني والمحتجين استمرت لساعات، إثر محاولة فتح الطرقات المقطوعة، ما أدى إلى إصابة العشرات من الطرفين.")
+txt = """يجري علماء في بريطانيا تجربة لاختبار فعالية عقار إيبوبروفين لمساعدة المصابين بفيروس كورونا. وذكرت هيئة الإذاعة البريطانية "بي بي سي" أن فريق مشترك من أطباء مستشفيات "جاي" و"سانت توماس" و"كينغز كوليدج" في لندن يعتقد أن إيبوبروفين، وهو مضاد للالتهابات ومسكن للألم، يمكن أن يعالج صعوبات التنفس.
+ويأمل العلماء أن يساعد هذا العلاج المنخفض التكلفة المرضى في الاستغناء عن أجهزة التنفس الصناعي. وذكرت أنه خلال فترة الاختبار، سيحصل نصف المرضى على إيبوبروفين بالإضافة إلى الرعاية المعتادة، حيث سيتم استخدام تركيبة خاصة من إيبوبروفين بدلا من الأقراص العادية التي قد يشتريها الناس عادة."""
+text = st.text_area("أدخل نص ليتم تلخيصه", value=txt)
 
 run_query = st.button("لخّص")
-if run_query: #and model_selected != 'Seq2Seq_LSTM':
+if run_query:
     # https://discuss.streamlit.io/t/showing-a-gif-while-st-spinner-runs/5084
     with st.spinner("جاري التلخيص ..."):
         result = get_results(text, model_selected, num_beams, length_penalty)
     if len(result) > 0:
-        #st.write("الملخص:")
         st.write(result)
     else:
         st.write("")
-#elif run_query and model_selected == 'Seq2Seq_LSTM':
-#    st.write("عذراً, هذا النموذج غير متاح حالياً")
